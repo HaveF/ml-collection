84fe2972461c424f1a7912a6aad6440239962e0f
leezhizhi@gmail.com <leezhizhi@gmail.com@86fef585-a735-c8ba-cdd1-e6cdd6190f71>
2012-03-21 12:56:42 +0000

    gmm added

--- trunk/Clustering/My_k_means/mykmeans_demo.m
+++ trunk/Clustering/My_k_means/mykmeans_demo.m
@@ -1,46 +1,6 @@
 function mykmeans_demo()
+clc;
 load('data.mat');
-mykmeans(x',4);
-end
-
-
-function [label] = mykmeans(X,k)
-    plot(X(:,1),X(:,2),'o');
-    colors = 'bgrcmyk';
-    hold on;
-    [s1 s2] = size(X);
-    xmin = min(X);
-    xmax = max(X);
-    label = zeros(1,s1);
-    clusters = zeros(k,s2);
-    for i= 1:k
-        for j = 1:s2
-            clusters(i,j) = xmin(j)+(xmax(j)-xmin(j))*rand();
-        end
-        hc(i) = plot(clusters(i,1),clusters(i,2),[colors(8-i) '.'],'MarkerSize', 40);
-    end
-    delta = 100;
-    ditance = zeros(1,k);
-    while delta>1e-8
-    oldclusters = clusters;
-    for i = 1:s1
-        for j=1:k
-            ditance(j) = norm(X(i,:)-oldclusters(j,:));
-        end
-        [iminvalue minidx ] = min(ditance);
-        label(i) = minidx;
-    end
-    %update clusters
-    for i=1:k
-        idx = find(label==i);
-        clusters(i,:) =  mean(X(idx,:));
-        delete(findobj(hc(i),'Type','Line')); 
-        plot(X(idx,1),X(idx,2),[colors(i) 'o']);
-        hc(i) = plot(clusters(i,1),clusters(i,2),[colors(8-i) '.'],'MarkerSize', 40);
-        
-    end
-    delta = norm(clusters - oldclusters)
-    pause(1)
-    end
-    
+reslt = mygmm(x',4);
+%mykmeans(x',4);
 end
